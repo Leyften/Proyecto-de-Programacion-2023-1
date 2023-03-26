509a5f6b5b9d78d9405cb5e6240b3b0aed82eb50
Leyften <128760645+Leyften@users.noreply.github.com>
2023-03-26 19:51:55 -0300

    Destacamiento

---
+++ ProyectoDeProgramacion_2023-1/src/pruebajavafx/Embarque.java
@@ -0,0 +1,54 @@
+
+package pruebajavafx;
+import java.util.*;
+/**
+ *
+ * @author pablo
+ */
+public class Embarque {
+    ArrayList<Caja> cajas;
+    
+    public Embarque(int tamano){
+        //Creacion array de cajas
+        cajas = new ArrayList<>(); 
+        //Relleno de cajas con randoms
+        Random rand = new Random();
+        
+        for(int i = 0; i < tamano; i++){
+            Caja caja = new Caja(rand.nextInt(100));
+            cajas.add(caja);
+        }
+        printArreglo();
+        insertionSort();
+        //printArreglo();
+          
+    }
+     
+    public void printArreglo(){
+        System.out.print("Arreglo: [ " );
+        for(int i = 0; i < cajas.size() - 1; i++){
+            System.out.print(cajas.get(i).getNum() + ", ");
+        }
+        System.out.println(cajas.get(cajas.size()-1).getNum() + "]");
+    }
+    
+    public void insertionSort(){
+        for(int i = 1; i < cajas.size(); i++){
+            int valorActual = cajas.get(i).getNum();
+            int j = i - 1;
+            while(j >= 0 && cajas.get(j).getNum() > valorActual){
+                System.out.println("-> "+valorActual);
+                System.out.println(cajas.get(j).getNum()+" - "+valorActual );
+                cajas.get(j+1).setNum(cajas.get(j).getNum());
+                j--;
+                cajas.get(j+1).setNum(valorActual);
+                printArreglo();
+                
+            }
+            //cajas.get(j+1).setNum(valorActual);
+            //Ultimo movimiento hecho por Nicolás
+            
+        }
+    }
+
+}
