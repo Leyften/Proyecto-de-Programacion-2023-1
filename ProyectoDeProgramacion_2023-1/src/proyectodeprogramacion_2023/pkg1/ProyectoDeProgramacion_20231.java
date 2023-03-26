501ad2cdd61dd711f1e5e94a4008869085485bec
Leyften <128760645+Leyften@users.noreply.github.com>
2023-03-25 23:44:34 -0300

    Se crea el archivo que contiene el programa

---
+++ ProyectoDeProgramacion_2023-1/src/proyectodeprogramacion_2023/pkg1/ProyectoDeProgramacion_20231.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectodeprogramacion_2023.pkg1;
+
+/**
+ *
+ * @author nikol
+ */
+public class ProyectoDeProgramacion_20231 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        // TODO code application logic here
+    }
+    
+}
