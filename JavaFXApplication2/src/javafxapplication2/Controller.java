4ac2bc3484644de6cffca407135a8208f671304b
Leyften <128760645+Leyften@users.noreply.github.com>
2023-04-15 18:20:04 -0400

    Cambio de logica al insertion

--- JavaFXApplication2/src/javafxapplication2/Controller.java
+++ JavaFXApplication2/src/javafxapplication2/Controller.java
@@ -21,18 +21,30 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.transform.Translate;
 import java.util.Collections;
+import javafx.animation.Interpolator;
 
 
 
 public class Controller implements Initializable{
-
+    
+    double cantidad=0;
+    int altura_Maxima=50;
+    //int ancho;
+    //int espacio;
+    int duracion = 500;
+    //int ARRAY_SIZE = 10; //cantidad
+    
+    Rectangle[] Arreglo_rectangulos = new Rectangle[100];
+    
+    int minimo=0;
+    int indice=0;
+    
+    
+    
     @FXML
     private AnchorPane anchorPane;
 
-    TranslateTransition transicion = new TranslateTransition();
-    TranslateTransition transicion2 = new TranslateTransition();
-    TranslateTransition transicion3 = new TranslateTransition();
-    TranslateTransition transicion4 = new TranslateTransition();
+    
 
     Random rand = new Random();
     
@@ -41,7 +53,6 @@ public class Controller implements Initializable{
     boolean temp = false;
     
     int posX = 50;
-    
     int posY = 250;
     
     @FXML
@@ -55,14 +66,14 @@ public class Controller implements Initializable{
     void start(ActionEvent event) {
         try{
             //int cantidad = (int) (this.text_usuario.getText());
-            double cantidad = Double.parseDouble(this.text_usuario.getText());
+            cantidad = Double.parseDouble(this.text_usuario.getText());
             if(cantidad>0 && (cantidad%1)==0){
-                if(this.temp == false){
+                if(temp == false){
                       crearRect((int) (cantidad-1));
-                      this.temp=true;
+                      temp=true;
                   }else{
-                      this.anchorPane.getChildren().removeAll(this.contenido);
-                      this.contenido.clear();               
+                      anchorPane.getChildren().removeAll(this.contenido);
+                      contenido.clear();               
                       crearRect((int )(cantidad-1));
                   }  
             }else{
@@ -85,189 +96,90 @@ public class Controller implements Initializable{
     }
     
     //Aqui se crea los rectangulos, se queria usar rotacion para dejar en buena posicion los rectangulos, al final no lo use
-    public void crearRect(int cantidad){
-        RotateTransition rotacion = new RotateTransition();
+    public void crearRect(int cantidad){       
         for (int i = 0; i <= cantidad; i++) {
-            Rectangle caja = new Rectangle(posX+(i*60), posY, 50, rand.nextInt(50) );
-            rotacion.setNode(caja);
-            rotacion.setDuration(Duration.millis(1));
-            rotacion.setByAngle(180);
-            rotacion.play();
-            this.contenido.add(caja);
-            anchorPane.getChildren().addAll(this.contenido.get(i));
-            this.contenido.get(i).setFill(Color.web("#2191FB"));
+            int altura = rand.nextInt(altura_Maxima);
+            Rectangle caja = new Rectangle(50,altura);
+            caja.setFill(Color.BLUEVIOLET);
+            caja.setTranslateX(i*(50+10));
+            caja.setTranslateY(altura_Maxima - altura);
+            
+            contenido.add(caja);
+            anchorPane.getChildren().addAll(contenido.get(i));
+            //Arreglo_rectangulos[i]=caja;
+            
         }
     }
-    //Metodo que no use
-    private void insertionSort(){        
-        for(int i = 0; i < this.contenido.size(); i++){
-            double valorActual;
-            int j = i - 1;
-            valorActual=this.contenido.get(i).getHeight();
-            //valorActual = Integer.parseInt(Double.toString(this.contenido.get(i).getHeight()));  
-            while(j >= 0 && this.contenido.get(j).getHeight()> valorActual){
-                desplazamiento(i,j);
-                
-                
-                 
-               
-                //this.contenido.get(j+1).setHeight(this.contenido.get(j).getHeight());
-                j--;
-            }
-            //this.contenido.get(j+1).setHeight(valorActual);
-        }    
-    }
-    //Todos los metodos @FXML se usan en la pantalla, este hace el insetion sort y es el que estaba trabajando para usarlo en la animacion
     @FXML
-    private void insertionSort2(){
-        for(int i = 0; i < this.contenido.size(); i++){
-            //System.out.println("i->"+i);
-            int valorActual;
-            int j = i - 1;
-            valorActual=(int) this.contenido.get(i).getHeight();
-            while(j >= 0 && this.contenido.get(j).getHeight()> valorActual){
-                
-                int k =i;
-                while(k>=0){
-                    desplazamiento(k,j);
-                    k--;
-                }
-                
-                //desplazamientoAislado(i);
-               
-                this.contenido.get(j+1).setHeight(this.contenido.get(j).getHeight());
-                //desplazamientoLateral(j);
-                j--;
-                //2 3 |1|
-                //  j  i
-                //2 3 > |1|
-                //       i
-                //  j > |1|
-                //2 >|1| 3
-                //j 
-                //1 2 3
-            }
+    private void insertionSort(){    
+        if (minimo >= cantidad) {
+            System.out.println("return");
+            return;
+        }
+        
+        Rectangle cajaMinimo = contenido.get(minimo);
+        Rectangle cajaIndice = contenido.get(indice);
+        
+        /*
+        Rectangle cajaMinimo = Arreglo_rectangulos[minimo];
+        Rectangle cajaIndice = Arreglo_rectangulos[indice];*/
+        
+        if (cajaIndice.getHeight() < cajaMinimo.getHeight()) {
+            minimo = indice;
             
-            this.contenido.get(j+1).setHeight(valorActual);
-            //desplazamientoReincorporadoH((j+1), i);
-           // desplazamientoReincorporadoV(i);
-        } 
+            indice++;
+        }
         
+        if (indice >= cantidad) {
+            mostrar(minimo, minimo, cajaMinimo, duracion);
+            minimo++;
+            indice = minimo;
+        } else {
+            mostrar(minimo, indice, cajaIndice, duracion);
+        }
         
-        
-    }
-    
-    //Solo un metodo de pruebas para crear rectangulos
-    public void crearM(){
-        Rectangle caja = new Rectangle(50, 250, 50, (50) );
-        caja.setFill(Color.web("#2191FB"));
-        this.contenido.add(caja);
-        Rectangle caja_1 = new Rectangle(110, 250, 50, 100 );
-        caja_1.setFill(Color.web("#2191FB"));
-        this.contenido.add(caja_1);
-        
-        anchorPane.getChildren().addAll(this.contenido);
-        
-    }
-    //Este metodo tiene la animacion más en general
-    public void desplazamiento(/*1*/int i,/*0*/ int j){
-        contenido.get(j).setFill(Color.web("#E6FA07"));
-        contenido.get(i).setFill(Color.web("#FA4439"));
-        transicion.setNode(contenido.get(i));
-        transicion.setDuration(Duration.millis(1000));
-        double temp = contenido.get(i).getX();
-        transicion.setByY(100);
-        //contenido.get(i).setY((contenido.get(i).getY())+100);
-        transicion.setByX(-60);
-        transicion.play();
-        
-        transicion2.setNode(contenido.get(j));
-        transicion2.setDelay(Duration.millis(1000));
-        transicion2.setDuration(Duration.millis(1000));
-        transicion2.setByX(temp-contenido.get(j).getX());
-        transicion2.play(); 
-        
-        transicion3.setNode(contenido.get(i));
-        transicion3.setDelay(Duration.millis(2000));
-        transicion3.setDuration(Duration.millis(1000));
-        transicion3.setByY(-1);
-        //contenido.get(i).setY(contenido.get(i).getY()-100);
-        transicion3.play();
-        
-        contenido.get(i).setFill(Color.web("#2191FB"));
-        contenido.get(j).setFill(Color.web("#2191FB"));
-        
-               
-    }
-    //De aqui son metodos especificos para la animacion, estaba tratando de hacerlo de otra forma comparado con el anterior
-    //Tomo una caja y la dejo de lado
-    public void desplazamientoAislado(int i){
-        transicion.setNode(contenido.get(i));
-        transicion.setDuration(Duration.millis(1000));
-        //double temp = contenido.get(i).getX();
-        transicion.setByY(100);
-        //contenido.get(i).setY((contenido.get(i).getY())+100);
-        //transicion.setByX(-60);
-        transicion.play();
-        contenido.get(i).setY(contenido.get(i).getY()+100);
-        
-    }
-    //las cajas anteriores las mueve hacia la derecha
-    public void desplazamientoLateral(int j){
-        transicion2.setNode(contenido.get(j));
-        transicion2.setDelay(Duration.millis(1000));
-        transicion2.setDuration(Duration.millis(1000));
-        double temp = contenido.get(j+1).getX();
-        transicion2.setByX(temp-contenido.get(j).getX());
-        transicion2.play();
-        contenido.get(j).setX(temp-contenido.get(j).getX());
-    }
-    //la caja aislada se mueve a la poscion correspondiente en el eje X
-    public void desplazamientoReincorporadoH(int j/*+1*/, int i){
-        transicion3.setNode(contenido.get(i));
-        transicion3.setDelay(Duration.millis(2000));
-        transicion3.setDuration(Duration.millis(1000));
-        transicion3.setByX(-(i-j)*60);
-        transicion3.play();
-        contenido.get(i).setX(contenido.get(i).getX()-((i-j)*60));
-    }
-    //la caja se mueve en el eje vertical para reicorporarse con las otras cajas
-    public void desplazamientoReincorporadoV(int i){
-        transicion4.setNode(contenido.get(i));
-        transicion4.setDelay(Duration.millis(2000));
-        transicion4.setDuration(Duration.millis(1000));
-        transicion4.setByY(-100);
-        //contenido.get(i).setY(contenido.get(i).getY()-100);
-        transicion4.play();
-        contenido.get(i).setY(contenido.get(i).getY()-100);
-    }
-    
-    //MOVIMIENTO QUE SE HACE MANUAL
-    public void movimientoM(){
-        contenido.get(0).setFill(Color.web("#E6FA07"));
-        contenido.get(1).setFill(Color.web("#FA4439"));
-        transicion.setNode(contenido.get(1));
-        transicion.setDuration(Duration.millis(1000));
-        double temp = contenido.get(1).getX();
-        transicion.setByY(100);
-        transicion.setByX(-10-posX);
-        transicion.play();
-        
-        transicion2.setNode(contenido.get(0));
-        transicion2.setDelay(Duration.millis(1000));
-        transicion2.setDuration(Duration.millis(1000));
-        transicion2.setByX(temp-contenido.get(0).getX());
-        transicion2.play(); 
+        cajaMinimo.setFill(Color.BLUEVIOLET);
 
-        transicion3.setNode(contenido.get(1));
-        transicion3.setDelay(Duration.millis(2000));
-        transicion3.setDuration(Duration.millis(1000));
-        transicion3.setByY(-1);
-        transicion3.play();
+        cajaIndice.setFill(Color.YELLOW);
+
+        cajaMinimo.toFront();
+
+        cajaIndice.toFront();
+
+        cajaMinimo.setFill(Color.BLUEVIOLET);
+
+        cajaIndice.setFill(Color.BLUEVIOLET);
+
+        cajaMinimo.toBack();
+
+        cajaIndice.toBack();
+
+        cajaMinimo.setFill(Color.RED);
+
+        insertionSort();
         
     }
        
+    private void mostrar(int indice1, int indice2, Rectangle caja1, int duracion) {
+        Rectangle caja2 = contenido.get(indice2);
+        //Rectangle caja2 = Arreglo_rectangulos[indice2];
 
+        TranslateTransition transition1 = new TranslateTransition(Duration.millis(duracion), caja1);
+        transition1.setByX((indice2 - indice1) * (50 + 10));
+        TranslateTransition transition2 = new TranslateTransition(Duration.millis(duracion), caja2);
+        transition2.setByX((indice1 - indice2) * (50 + 10));
+
+        transition1.play();
+        transition2.play();
+        
+        contenido.set(indice1, caja2);
+        contenido.set(indice2, caja1);
+
+        /*
+        Arreglo_rectangulos[indice1] = caja2;
+        Arreglo_rectangulos[indice2] = caja1;*/
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {/*
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.1), e->{
