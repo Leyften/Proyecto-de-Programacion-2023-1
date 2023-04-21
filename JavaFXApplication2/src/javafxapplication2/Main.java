9f9ab5e166698fe2893cc8f776c28f29334b9e2e
estudiantedesesperado <128736602+estudiantedesesperado@users.noreply.github.com>
2023-04-21 16:54:40 -0400

    Inclusion de fondo
    dolor y sufrimiento

--- JavaFXApplication2/src/javafxapplication2/Main.java
+++ JavaFXApplication2/src/javafxapplication2/Main.java
@@ -1,21 +1,39 @@
 package javafxapplication2;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
+    public void start(Stage primaryStage) throws Exception {
+    // cargar la vista FXML
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    InputStream stream = new FileInputStream("C:\\Users\\augus\\OneDrive\\Escritorio\\Nueva carpeta (3)\\Proyecto-de-Programacion-2023-1-Prototipo\\JavaFXApplication2\\src\\imagen\\fondomario.png");
 
+    // crear un objeto ImageView con la imagen deseada
+    Image image = new Image(stream);
+    ImageView imageView = new ImageView(image);
+    
+    // crear un contenedor para la imagen y la vista FXML
+    AnchorPane anchorPane = new AnchorPane();
+    anchorPane.getChildren().addAll(imageView, root); // agregar la imagen y la vista al contenedor
+    Scene scene = new Scene(anchorPane); // crear una escena con el contenedor como raíz
+    
+    primaryStage.setTitle("Imagen");
+    primaryStage.setScene(scene);
+    primaryStage.sizeToScene();
+    primaryStage.show();
+}
 
     public static void main(String[] args) {
         launch(args);
--- JavaFXApplication2/src/javafxapplication2/NumBuilder.java
+++ JavaFXApplication2/src/javafxapplication2/NumBuilder.java
@@ -7,194 +7,185 @@ package javafxapplication2;
 import java.util.ArrayList;
 import javafx.scene.shape.Line;
 
-
 public class NumBuilder {
+
     ArrayList<ArrayList<Line>> digitos = new ArrayList<>();
     ArrayList<Line> digitosLines = new ArrayList<>();
 
     public NumBuilder(int num) {
-        if (num < 10){
+        if (num < 10) {
             //En caso de que el numero ingresado sea de solo un digito
-            
+
             digitos.add(Dibujar(num));
-            
-            for(int i = 0; i < digitos.get(0).size(); i++){
-                digitos.get(0).get(i).setStartX((digitos.get(0).get(i).getStartX())-80);
+
+            for (int i = 0; i < digitos.get(0).size(); i++) {
+                digitos.get(0).get(i).setStartX((digitos.get(0).get(i).getStartX()) - 80);
             }
             digitosLines.clear();
-        }
-        else{
-            
-            int aux = num/10;
+        } else {
+
+            int aux = num / 10;
             digitos.add(Dibujar(aux));
             digitosLines.clear();
-            
-            digitos.add(Dibujar((aux*10)-num));
+
+            digitos.add(Dibujar((aux * 10) - num));
             digitos.add(Dibujar(aux));
             digitosLines.clear();
-            
+
         }
 
-        
-
-        
-        
     }
 
-    private ArrayList<Line> Dibujar(int op){
+    private ArrayList<Line> Dibujar(int op) {
         Line linea;
         Line linea2;
         Line linea3;
         Line linea4;
         Line linea5;
-        
-        switch(op){
+
+        switch (op) {
             case 1:
                 linea = new Line(0.0f, 50.0f, 80.0f, 0.0f);
                 linea2 = new Line(80.0f, 0.0f, 80.0f, 100.0f);
-               
-                
+
                 digitosLines.add(linea);
                 digitosLines.add(linea2);
-                
+
                 return digitosLines;
-                
+
             case 2:
-                
+
                 linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
                 linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
                 linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
                 linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
                 linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                
+
                 digitosLines.add(linea);
                 digitosLines.add(linea2);
                 digitosLines.add(linea3);
                 digitosLines.add(linea4);
                 digitosLines.add(linea5);
-                
-                return digitosLines; 
-                
-            case 3:
-                
-                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                
-                digitosLines.add(linea);
-                digitosLines.add(linea2);
-                digitosLines.add(linea3);
-                digitosLines.add(linea4);
-                
-                return digitosLines; 
-                
-            case 4: 
-                
-                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                
-                digitosLines.add(linea);
-                digitosLines.add(linea2);
-                digitosLines.add(linea3);
-                
-                return digitosLines; 
-                
-            case 5: 
-                
-                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                
-                digitosLines.add(linea);
-                digitosLines.add(linea2);
-                digitosLines.add(linea3);
-                digitosLines.add(linea4);
-                digitosLines.add(linea5);
-                
+
                 return digitosLines;
-                
-            case 6: 
-                
+
+            case 3:
+
                 linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
                 linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
                 linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
                 linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                
-                digitosLines.add(linea);
-                digitosLines.add(linea2);
-                digitosLines.add(linea3);
-                digitosLines.add(linea4);
-                digitosLines.add(linea5);
-                
-                return digitosLines; 
-            case 7:
-                
-                linea = new Line(0.0f, 0.0f, 80.0f, 0.0f);
-                linea2 = new Line(80.0f, 0.0f, 80.0f, 100.0f);
-                
-                digitosLines.add(linea);
-                digitosLines.add(linea2);
-                
-                return digitosLines; 
-                
-            case 8:
-                
-                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                
-                digitosLines.add(linea);
-                digitosLines.add(linea2);
-                digitosLines.add(linea3);
-                digitosLines.add(linea4);
-                digitosLines.add(linea5);
-                
-                return digitosLines; 
-            case 9:
-                
-                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                
-                digitosLines.add(linea);
-                digitosLines.add(linea2);
-                digitosLines.add(linea3);
-                digitosLines.add(linea4);
-                digitosLines.add(linea5);
-                
-                return digitosLines; 
-            case 0:
-                
-                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
-                
+
                 digitosLines.add(linea);
                 digitosLines.add(linea2);
                 digitosLines.add(linea3);
                 digitosLines.add(linea4);
 
-                return digitosLines; 
+                return digitosLines;
+
+            case 4:
+
+                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+
+                digitosLines.add(linea);
+                digitosLines.add(linea2);
+                digitosLines.add(linea3);
+
+                return digitosLines;
+
+            case 5:
+
+                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+
+                digitosLines.add(linea);
+                digitosLines.add(linea2);
+                digitosLines.add(linea3);
+                digitosLines.add(linea4);
+                digitosLines.add(linea5);
+
+                return digitosLines;
+
+            case 6:
+
+                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+
+                digitosLines.add(linea);
+                digitosLines.add(linea2);
+                digitosLines.add(linea3);
+                digitosLines.add(linea4);
+                digitosLines.add(linea5);
+
+                return digitosLines;
+            case 7:
+
+                linea = new Line(0.0f, 0.0f, 80.0f, 0.0f);
+                linea2 = new Line(80.0f, 0.0f, 80.0f, 100.0f);
+
+                digitosLines.add(linea);
+                digitosLines.add(linea2);
+
+                return digitosLines;
+
+            case 8:
+
+                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+
+                digitosLines.add(linea);
+                digitosLines.add(linea2);
+                digitosLines.add(linea3);
+                digitosLines.add(linea4);
+                digitosLines.add(linea5);
+
+                return digitosLines;
+            case 9:
+
+                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea5 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+
+                digitosLines.add(linea);
+                digitosLines.add(linea2);
+                digitosLines.add(linea3);
+                digitosLines.add(linea4);
+                digitosLines.add(linea5);
+
+                return digitosLines;
+            case 0:
+
+                linea = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea2 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea3 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+                linea4 = new Line(10.0f, 10.0f, 200.0f, 140.0f);
+
+                digitosLines.add(linea);
+                digitosLines.add(linea2);
+                digitosLines.add(linea3);
+                digitosLines.add(linea4);
+
+                return digitosLines;
         }
         return digitosLines;
-        
+
     }
-    
+
     public ArrayList<ArrayList<Line>> getDigitos() {
         return digitos;
     }
-    
-    
+
 }
-
-
