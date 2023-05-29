800a430336c1b5b70413fb4d9c1ccf85aaeea4b4
estudiantedesesperado <aufuenzalida20@alumnos.utalca.cl>
2023-05-29 01:47:32 -0400

    grua buena

--- JavaFXApplication2/src/javafxapplication2/grua.java
+++ JavaFXApplication2/src/javafxapplication2/grua.java
@@ -1,28 +1,34 @@
-
 package javafxapplication2;
 
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import java.util.ArrayList;
 
-public class grua {
+public class Grua {
 
     private ArrayList<Node> creargrua;
 
-    public grua() {
+    public Grua(int par) {
         // Crear un círculo para el gancho
-        Circle gancho = new Circle(50, 700, 10);
+        Line gancho1 = new Line(par, 500, par+8,505);
+        Line gancho2 = new Line(par+8, 505, par+8,513);
+        Line gancho3 = new Line(par+8, 513, par,518);
+        Line gancho4 = new Line(par, 518, par-8,513);
+        Line gancho5 = new Line(par-8, 513, par-8,506);
 
         // Crear una línea para la cuerda
-        Line cuerda = new Line(50, 60, 50, 700);
+        Line cuerda = new Line(par, 60, par,500);
 
         // Crear una instancia de ArrayList
         creargrua = new ArrayList<>();
 
         // Agregar el círculo y la línea al ArrayList
-        creargrua.add(gancho);
+        creargrua.add(gancho5);
+        creargrua.add(gancho4);
+        creargrua.add(gancho3);
+        creargrua.add(gancho2);
+        creargrua.add(gancho1);
         creargrua.add(cuerda);
     }
 
@@ -33,4 +39,9 @@ public class grua {
 
         return pane;
     }
+    
+    public void eliminarGrua() {
+        creargrua.clear();
+    }
+    
 }
